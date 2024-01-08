0ed410e63bc987cf0e36a4985618b1eeac1d83eb
emremert <emre.mrt98@gmail.com>
2024-01-09 02:59:53 +0300

    Create rn files

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Selamlar</Text>
+      <Text style={styles.text2}>Hello World</Text>
+      <Text style={{flex: 1, backgroundColor: 'white', fontSize: 36}}>Inline Css</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'red',
     justifyContent: 'center',
+  },
+  text: {
+    flex: 1,
+    backgroundColor: 'white',
+    fontSize: 24,
+    color: 'gray',
+  },
+  text2: {
+    flex: 3,
+    backgroundColor: 'blue',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#343a40',
   },
 });
