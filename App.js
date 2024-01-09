d782a00c86becd2cde213c681f2b89af85b8fe8c
emremert <emre.mrt98@gmail.com>
2024-01-09 04:03:16 +0300

    Create nativewind and configuration

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import HomeScreen from './src/screen/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Selamlar</Text>
-      <Text style={styles.text2}>Hello World</Text>
-      <Text style={{flex: 1, backgroundColor: 'white', fontSize: 36}}>Inline Css</Text>
+    <View className="flex-1">
+      <HomeScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-  },
-  text: {
-    flex: 1,
-    backgroundColor: 'white',
-    fontSize: 24,
-    color: 'gray',
-  },
-  text2: {
-    flex: 3,
-    backgroundColor: 'blue',
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#343a40',
-  },
-});
---
+++ src/screen/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+    return (
+        <View className="bg-red-600 flex-1 py-20">
+            <Text className="text-[37px]">HomeScreen</Text>
+        </View>
+    )
+}
+
