c128593d83623e11f3e5001b713232ae2e0d1f71
emremert <emre.mrt98@gmail.com>
2024-01-25 00:56:09 +0300

    Complete Header for home screen

---
+++ src/components/home/Header.jsx
@@ -0,0 +1,31 @@
+import { Text, View, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons';
+
+
+export default function Header() {
+    return (
+        <View className='bg-white flex-row justify-between p-[10px] items-center'>
+            <View className='flex-row gap-[7px] items-center'>
+                <View>
+                    <Image
+                        className=''
+                        source={require('../../../assets/images/user.jpg')}
+                    />
+                </View>
+                <View>
+                    <Text className='text-primary font-medium mb-[3px]'>Hoşgeldin Emre</Text>
+                    <Text className='text-secondary'>Üye</Text>
+                </View>
+            </View>
+            <TouchableOpacity>
+                <View className='relative w-[36px] h-[36px] bg-[#F8F9FA] justify-center items-center rounded-full'>
+                    <Feather name="bell" size={20} color="#343A40" />
+                    <View className='absolute right-1 top-1 bg-[#ff3200] justify-center items-center rounded-full w-[13px] h-[13px] '>
+                        <Text className='text-[10px] text-white' >1</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
