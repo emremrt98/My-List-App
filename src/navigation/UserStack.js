1301fb097b215bc2c4e609a3d7fe3faf61efda9d
emremert <emre.mrt98@gmail.com>
2024-01-25 00:33:12 +0300

    Complete bottom bar navigation for home screen

--- src/navigation/UserStack.js
+++ src/navigation/UserStack.js
@@ -1,11 +1,106 @@
-import { Text, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen, Profile, AddTaskScreen, CompletedScreen, TaskScreen } from '../screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { AntDesign, FontAwesome5, Entypo, MaterialIcons } from '@expo/vector-icons';
+import { Text } from 'react-native';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function UserStack() {
     return (
-        <View>
-            <Text>UserStack</Text>
-        </View>
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+                <Tab.Screen name="Home" component={HomeScreen}
+                    options={{
+                        tabBarLabel: ({ focused }) => (
+                            focused
+                                ?
+                                <Text className='text-[11px] text-main font-medium'>Ana Sayfa</Text>
+                                :
+                                <Text className='text-[11px] text-secondary'>Ana Sayfa</Text>
+                        ),
+                        tabBarIcon: ({ focused }) => (
+                            focused
+                                ?
+                                <AntDesign name="home" size={20} color="#3B5BDB" />
+                                :
+                                <AntDesign name="home" size={20} color="#868E96" />
+                        ),
+                    }}
+
+                />
+
+
+                <Tab.Screen name="Task" component={TaskScreen}
+                    options={{
+                        tabBarLabel: ({ focused }) => (
+                            focused
+                                ?
+                                <Text className='text-[11px] text-main font-medium'>Tasklarım</Text>
+                                :
+                                <Text className='text-[11px] text-secondary'>Tasklarım</Text>
+                        ),
+                        tabBarIcon: ({ focused }) => (
+                            focused
+                                ?
+                                <FontAwesome5 name="tasks" size={20} color="#3B5BDB" />
+                                :
+                                <FontAwesome5 name="tasks" size={20} color="#868E96" />
+                        ),
+                    }}
+
+                />
+                <Tab.Screen name="AddTask" component={AddTaskScreen}
+                    options={{
+                        tabBarLabel: () => (<Text className='text-[11px] text-main font-medium'>Task Ekle</Text>),
+                        tabBarIcon: () => (<Entypo name="squared-plus" size={24} color="#3B5BDB" />),
+                    }}
+                />
+                <Tab.Screen name="Completed" component={CompletedScreen}
+                    options={{
+                        tabBarLabel: ({ focused }) => (
+                            focused
+                                ?
+                                <Text className='text-[11px] text-main font-medium'>Tamamlanan</Text>
+                                :
+                                <Text className='text-[11px] text-secondary'>Tamamlanan</Text>
+                        ),
+                        tabBarIcon: ({ focused }) => (
+                            focused
+                                ?
+                                <MaterialIcons name="playlist-add-check" size={28} color="#3B5BDB" />
+                                :
+                                <MaterialIcons name="playlist-add-check" size={28} color="#868E96" />
+                        ),
+                    }}
+
+                />
+                <Tab.Screen name="Profile" component={Profile}
+                    options={{
+                        tabBarLabel: ({ focused }) => (
+                            focused
+                                ?
+                                <Text className='text-[11px] text-main font-medium'>Profil</Text>
+                                :
+                                <Text className='text-[11px] text-secondary'>Profil</Text>
+                        ),
+                        tabBarIcon: ({ focused }) => (
+                            focused
+                                ?
+                                <FontAwesome5 name="user" size={20} color="#3B5BDB" />
+                                :
+                                <FontAwesome5 name="user" size={20} color="#868E96" />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
+
+
+
+
 
--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function HomeScreen() {
     return (
-        <View className="bg-red-600 flex-1 py-20">
+        <View className="bg-red-600 flex-1">
             <Text className="text-[37px]">HomeScreen</Text>
         </View>
     )
